78ea9f3066cae87c9a884f259a6b4f9b0dada067
Andrea Speziale <andreaspeziale@andreas-mbp.homenet.telecomitalia.it>
2018-04-06 21:57:03 +0200

    Will add more tests about the deployed contract and the failures of the split public function

--- test/splitter.js
+++ test/splitter.js
@@ -9,14 +9,67 @@ contract('Splitter', function(accounts) {
 
     // deploy
     beforeEach(function() {
-        return Splitter.new({from: ownerOrSender}).then(function(instance){
-             contract = instance;
+        return Splitter.new({from: ownerOrSender}).then(function(_instance){
+             contract = _instance;
         })
     })
 
     it("should be own by owner", function(){
         return contract.owner({from: ownerOrSender}).then(function(_owner){
-            assert.strictEqual(_owner, ownerOrSender, "Contract owner is not the same")
+            assert.strictEqual(_owner, ownerOrSender, "Contract owner is not the same");
+        })
+    })
+
+    it("should not be created passing a value", function(){
+        return Splitter.new({from: ownerOrSender, value: 10}).then(function(_instance){
+            //
+        }).catch(err => assert.include(err.message, 'non-payable constructor', 'Contract cannot be created with value'))
+    })
+
+    describe("testing split function", function() {
+        describe("failing cases", function(){
+
+            it("should fail if the owner/sender is second_recipient", function(){
+                return contract.split(first_recipient, ownerOrSender, {from: ownerOrSender, value: 1})
+                    .then(function(txn){
+                        //
+                    }).catch(err => assert.include(err.message, 'revert', 'state reverted because of owner is one of the recipient'))
+            })
+
+            it("should fail if the owner/sender is first_recipient", function(){
+                return contract.split(ownerOrSender, second_recipient, {from: ownerOrSender, value: 1})
+                    .then(function(txn){
+                        //
+                    }).catch(err => assert.include(err.message, 'revert', 'state reverted because of owner is one of the recipient'))
+            })
+
+            it("should fail if the recipients are the same", function(){
+                return contract.split(second_recipient, second_recipient, {from: ownerOrSender, value: 1})
+                    .then(function(txn){
+                        //
+                    }).catch(err => assert.include(err.message, 'revert', 'state reverted because the address are the same'))
+            })
+
+            it("should fail if the first_recipient is empty", function(){
+                return contract.split('', second_recipient, {from: ownerOrSender, value: 1})
+                    .then(function(txn){
+                        //
+                    }).catch(err => assert.include(err.message, 'revert', 'state reverted because the first_recipient is empty'))
+            })
+
+            it("should fail if the second_recipient is empty", function(){
+                return contract.split(first_recipient, '', {from: ownerOrSender, value: 1})
+                    .then(function(txn){
+                        //
+                    }).catch(err => assert.include(err.message, 'revert', 'state reverted because the second_recipient is empty'))
+            })
+
+            it("should fail if the recipients are empty", function(){
+                return contract.split('', '', {from: ownerOrSender, value: 1})
+                    .then(function(txn){
+                        //
+                    }).catch(err => assert.include(err.message, 'revert', 'state reverted because the empty address'))
+            })
         })
     })
 })
