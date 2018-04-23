7ca15533d15873d1a15ada5bfb53bbb836f0cb0f
andreaspeziale <andreaf.speziale@gmail.com>
2018-04-23 16:41:03 +0200

    Test updates

--- test/splitter.js
+++ test/splitter.js
@@ -17,11 +17,9 @@ contract('Splitter', function(accounts) {
         return ev
     }
 
-    // deploy
     beforeEach(async() => {
-        contract = await Splitter.new(false)
+        contract = await Splitter.new(false)        
     })
-
     
     // I expected it would fail but it isn't
     // it("should not be created passing a value", async () => {
@@ -102,193 +100,78 @@ contract('Splitter', function(accounts) {
                 const split = await contract.split(firsRecipient, firsRecipient, {from: ownerOrSender, value: 1})
                 assert.isUndefined(split, 'The split process can accept same recipients')
             } catch(e) {
-                console.log(e)
                 assert.include(e.message, 'revert', 'The split process cannot accept same recipients');
             }
         })
+        it("should fail if the the recipient is empty", async () => {
+            try {
+                const split = await contract.split(firsRecipient, '0x0000000000000000000000000000000000000000', {from: ownerOrSender, value: 1})
+                assert.isUndefined(split, 'The split process can accept empty recipients')
+            } catch(e) {
+                assert.include(e.message, 'revert', 'The split process cannot accept empty recipients');
+            }
+        })
+        it("should fail because of amount = 0", async () => {
+            try {
+                const split = await contract.split(firsRecipient, secondRecipient, {from: ownerOrSender, value: 0})
+                assert.isUndefined(split, 'The split process can accept 0 as msg.value')
+            } catch(e) {
+                assert.include(e.message, 'revert', 'The split process cannot accept 0 as msg.value');
+            }
+        })
+        it("should fail because of odd amounts", async () => {
+            try {
+                const split = await contract.split(firsRecipient, secondRecipient, {from: ownerOrSender, value: 1})
+                assert.isUndefined(split, 'The split process can accept odd amounts')
+            } catch(e) {
+                assert.include(e.message, 'revert', 'The split process cannot accept odd amounts');
+            }
+        })
+        it("should not change recipients balances", async () => {
+            const firstInitial = web3.eth.getBalance(firsRecipient)
+            const secondInitial = web3.eth.getBalance(secondRecipient)
+            const split = await contract.split(firsRecipient, secondRecipient, {from: ownerOrSender, value: web3.toWei('4', 'ether')})
+            const firstFinal = web3.eth.getBalance(firsRecipient)
+            const secondFinal = web3.eth.getBalance(secondRecipient)
+            assert.strictEqual(firstInitial.toString(10), firstFinal.toString(10), "firsRecipient has a changed balance")
+            assert.strictEqual(secondInitial.toString(10), secondFinal.toString(10), "secondRecipient has a changed balance")
+        })
+        it("should set 2 ehter to be withdraw for both the recipients", async () => {
+            const split = await contract.split(firsRecipient, secondRecipient, {from: ownerOrSender, value: web3.toWei('4', 'ether')})
+            const balances = await contract.balances(firsRecipient)
+            const balances2 = await contract.balances(secondRecipient)
+            assert.strictEqual(balances.toString(10), web3.toWei('2', 'ether').toString(10), "firsRecipient has not 2 ether ready for withdraw")
+            assert.strictEqual(balances2.toString(10), web3.toWei('2', 'ether').toString(10), "firsRecipient has not 2 ether ready for withdraw")
+        })
+        it("should fire Split event", async () => {
+            const split = await contract.split(firsRecipient, secondRecipient, {from: ownerOrSender, value: web3.toWei('4', 'ether')})
+            const ev = expectEvent(split, 'Split')
+            expect(ev.args.from).to.equal(ownerOrSender)
+            expect(ev.args.firstRecipient).to.equal(firsRecipient)
+            expect(ev.args.secondRecipient).to.equal(secondRecipient)
+            expect(ev.args.amount.toString(10)).to.equal(web3.toWei('2', 'ether'))
+        })
+
+        it("should find 0 ehter to be withdraw by thirdParty", async () => {
+            const split = await contract.split(firsRecipient, secondRecipient, {from: ownerOrSender, value: web3.toWei('4', 'ether')})
+            const balances = await contract.balances(thirdParty)
+            assert.strictEqual(balances.toString(10), web3.toWei('0', 'ether').toString(10), "thirdParty has not 0 ether ready for withdraw")
+        })
     })
-
-    // describe("Testing split public function:", function() {
-
-    //     describe("Failing cases:", function(){
-
-    //         it("should fail if the owner/sender is second_recipient", function(){
-    //             return contract.split(first_recipient, owner_or_sender, {from: owner_or_sender, value: 1})
-    //                 .then(function(_txn){
-    //                     //
-    //                 }).catch(err => assert.include(err.message, 'revert'))
-    //         })
-
-    //         it("should fail if the owner/sender is first_recipient", function(){
-    //             return contract.split(owner_or_sender, second_recipient, {from: owner_or_sender, value: 1})
-    //                 .then(function(_txn){
-    //                     //
-    //                 }).catch(err => assert.include(err.message, 'revert'))
-    //         })
-
-    //         it("should fail if the recipients are the same", function(){
-    //             return contract.split(second_recipient, second_recipient, {from: owner_or_sender, value: 1})
-    //                 .then(function(_txn){
-    //                     //
-    //                 }).catch(err => assert.include(err.message, 'revert'))
-    //         })
-
-    //         it("should fail if the first_recipient is empty", function(){
-    //             return contract.split('', second_recipient, {from: owner_or_sender, value: 1})
-    //                 .then(function(_txn){
-    //                     //
-    //                 }).catch(err => assert.include(err.message, 'revert'))
-    //         })
-
-    //         it("should fail if the second_recipient is empty", function(){
-    //             return contract.split(first_recipient, '', {from: owner_or_sender, value: 1})
-    //                 .then(function(_txn){
-    //                     //
-    //                 }).catch(err => assert.include(err.message, 'revert'))
-    //         })
-
-    //         it("should fail if the recipients are empty", function(){
-    //             return contract.split('', '', {from: owner_or_sender, value: 1})
-    //                 .then(function(_txn){
-    //                     //
-    //                 }).catch(err => assert.include(err.message, 'revert'))
-    //         })
-
-    //         it("should fail because of amount = 0", function(){
-    //             return contract.split(first_recipient, second_recipient, {from: owner_or_sender, value: 0})
-    //                 .then(function(_txn){
-    //                     //
-    //                 }).catch(err => assert.include(err.message, 'revert'))
-    //         })
-
-    //         it("should fail because of amount % 2 != 0", async () => {
-    //             try {
-    //                 const split = contract.split(first_recipient, second_recipient, {from: owner_or_sender, value: 1})
-    //                 // console.log(split)
-    //             } catch(e) {console.error(e)}
-    //         })
-    //     })
-
-    //     // ToDo async and flat by chaining
-
-    //     describe("Successful cases:", function(){
-    //         it("should not change recipients balances", function(){
-    //                         var first_recipient_initial_balance = web3.eth.getBalance(first_recipient)
-    //                         var second_recipient_initial_balance = web3.eth.getBalance(second_recipient)
-
-    //                         return contract.split(first_recipient, second_recipient, {from: owner_or_sender, value: 2})
-    //                             .then(_res => {
-    //                                 var first_recipient_final_balance = web3.eth.getBalance(first_recipient)
-    //                                 var second_recipient_final_balance = web3.eth.getBalance(second_recipient)
-                                    
-    //                                 assert.strictEqual(first_recipient_initial_balance.toString(10), first_recipient_final_balance.toString(10), "first_recipient has a changed balance")
-    //                                 assert.strictEqual(second_recipient_initial_balance.toString(10), second_recipient_final_balance.toString(10), "second_recipient has a changed balance")
-    //                             })
-    //                     })
-
-    //         it("should remove 2 from sender balance", function(){
-    //             var owner_or_sender_initial_balance = web3.eth.getBalance(owner_or_sender)
-
-    //             return contract.split(first_recipient, second_recipient, {from: owner_or_sender, value: 2})
-    //                 .then(_res => {
-
-    //                     var owner_or_sender_final_balance = web3.eth.getBalance(owner_or_sender)
-
-    //                     var gas_used = _res.receipt.gasUsed
-    //                     var tx = web3.eth.getTransaction(_res.tx)
-    //                     var gas_price = tx.gasPrice
-
-    //                     assert.strictEqual(owner_or_sender_final_balance.toString(10), owner_or_sender_initial_balance.minus(2).minus(gas_price.mul(gas_used)).toString(10), "second_recipient has not gotten the correct amount")
-    //                 })
-    //         })
-
-    //         it("should map first_recipient address to balance 1", function(){
-    //             return contract.split(first_recipient, second_recipient, {from: owner_or_sender, value: 2})
-    //                 .then(_res => {
-    //                     return  contract.balances(first_recipient, {from: owner_or_sender})
-    //                         .then(_balance => {
-    //                             assert.strictEqual(_balance.toString(10), "1", "first_recipient not mapped to balance = 1")
-    //                         })
-    //                 })
-    //         })
-
-    //         it("should map second_recipient address to balance 1", function(){
-    //             return contract.split(first_recipient, second_recipient, {from: owner_or_sender, value: 2})
-    //                 .then(_res => {
-    //                     return  contract.balances(second_recipient, {from: owner_or_sender})
-    //                         .then(_balance => {
-    //                             assert.strictEqual(_balance.toString(10), "1", "second_recipient not mapped to balance = 1")
-    //                         })
-    //                 })
-    //         })
-
-    //     })
-    //     /*
-    //     describe("Successful cases:", function(){
-
-    //         it("should split correctly", function(){
-    //             var first_recipient_initial_balance = web3.eth.getBalance(first_recipient)
-    //             var second_recipient_initial_balance = web3.eth.getBalance(second_recipient)
-    //             var owner_or_sender_initial_balance = web3.eth.getBalance(owner_or_sender)
-
-    //             return contract.split(first_recipient, second_recipient, {from: owner_or_sender, value: 2})
-    //                 .then(_res => {
-    //                     var first_recipient_final_balance = web3.eth.getBalance(first_recipient)
-    //                     var second_recipient_final_balance = web3.eth.getBalance(second_recipient)
-    //                     var owner_or_sender_final_balance = web3.eth.getBalance(owner_or_sender)
-
-    //                     var gas_used = _res.receipt.gasUsed
-    //                     var tx = web3.eth.getTransaction(_res.tx)
-    //                     var gas_price = tx.gasPrice
-
-    //                     assert.strictEqual(first_recipient_final_balance.toString(10), first_recipient_initial_balance.plus(1).toString(10), "first_recipient has not gotten the correct amount")
-    //                     assert.strictEqual(second_recipient_final_balance.toString(10), second_recipient_initial_balance.plus(1).toString(10), "second_recipient has not gotten the correct amount")
-    //                     assert.strictEqual(owner_or_sender_final_balance.toString(10), owner_or_sender_initial_balance.minus(2).minus(gas_price.mul(gas_used)).toString(10), "second_recipient has not gotten the correct amount")
-    //                 })
-    //         })
-
-    //         it("should split correctly", function(){
-    //             return contract.split.call(first_recipient, second_recipient, {from: owner_or_sender, value: 2})
-    //                 .then(_res => {
-    //                     assert.strictEqual(_res, true, "is not splitting correctly")
-    //                 })
-    //         })
-    //     })*/
-
-    //     //ToDo check on inherited contracts functionalities
-    // })
-    // describe("Testing withdrawal public function:", function() {
-    //     describe("Failing cases:", function(){
-    //         it("should not increment the recipient balance", function(){
-    //             //
-    //         })
-    //     })
-    //     describe("Successful cases:", function(){
-    //         it("should set recipient withdraw = 0", function(){
-    //             var first_recipient_initial_balance = web3.eth.getBalance(first_recipient)
-    //             return contract.split(first_recipient, second_recipient, {from: owner_or_sender, value: 2})
-    //                 .then(_res => {
-    //                     return contract.withdraw({from: first_recipient})
-    //                         .then(_res => {
-    //                             return  contract.balances(first_recipient, {from: owner_or_sender})
-    //                                 .then(_balance => {
-    //                                     assert.strictEqual(_balance.toString(10), "0", "first_recipient pending withdraw is not 0")
-    //                                 })
-    //                         })
-    //                 })
-    //         })
-    //         it("should increment the recipients balance", function(){
-    //             var first_recipient_initial_balance = web3.eth.getBalance(first_recipient)
-    //             return contract.split(first_recipient, second_recipient, {from: owner_or_sender, value: 2})
-    //                 .then(_res => {
-    //                     return contract.withdraw({from: first_recipient})
-    //                         .then(_res => {
-    //                             // console.log(_res.receipt.logs[0].args)
-    //                             var first_recipient_final_balance = web3.eth.getBalance(first_recipient)
-    //                             expect(first_recipient_final_balance.toNumber()).to.be.gt(first_recipient_initial_balance)
-    //                         })
-    //                 })
-    //         })
-    //     })
-    // })
+    describe("Withdraw public function:", async () => {
+        it("should fire Withdraw event", async () => {
+            const split = await contract.split(firsRecipient, secondRecipient, {from: ownerOrSender, value: web3.toWei('4', 'ether')})
+            const withdraw = await contract.withdraw({from: firsRecipient})
+            const ev = expectEvent(withdraw, 'Withdraw')
+            expect(ev.args.from).to.equal(firsRecipient)
+            expect(ev.args.amount.toString(10)).to.equal(web3.toWei('2', 'ether'))
+        })
+        it("should increase recipient balance", async () => {
+            const initial = web3.eth.getBalance(firsRecipient)
+            const split = await contract.split(firsRecipient, secondRecipient, {from: ownerOrSender, value: web3.toWei('4', 'ether')})
+            const withdraw = await contract.withdraw({from: firsRecipient})
+            const final = web3.eth.getBalance(firsRecipient)
+            assert.isAbove(final, initial, "firsRecipient balance is not greater than before the withdraw request")
+        })
+    })
 })
