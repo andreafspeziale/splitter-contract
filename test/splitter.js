d7da90345c3acb476c6c2ba0d74822adf9ced97a
andreaspeziale <andreaf.speziale@gmail.com>
2018-04-30 21:00:13 +0200

    Add more test for split and withdraw

--- test/splitter.js
+++ test/splitter.js
@@ -1,13 +1,11 @@
-// ToDo move to const
-
-const Splitter = artifacts.require("./Splitter.sol")
+const Splitter = artifacts.require('./Splitter.sol')
 const _ = require('lodash')
 
 contract('Splitter', function(accounts) {
 
     let contract
     const ownerOrSender = accounts[0]
-    const firsRecipient = accounts[1]
+    const firstRecipient = accounts[1]
     const secondRecipient = accounts[2]
     const thirdParty = accounts[3]
 
@@ -21,8 +19,10 @@ contract('Splitter', function(accounts) {
         contract = await Splitter.new(false)        
     })
     
+    // https://github.com/trufflesuite/truffle/issues/917
+
     // I expected it would fail but it isn't
-    // it("should not be created passing a value", async () => {
+    // it('should not be created passing a value', async () => {
     //     try {
     //         const split = await Splitter.new('ciao')
     //         console.log(split)
@@ -31,19 +31,19 @@ contract('Splitter', function(accounts) {
     //     }
     // })
 
-    describe("Ownership stuff:", async () => {
-        it("should be own by owner", async () => {
+    describe('Ownership stuff:', async () => {
+        it('should be own by owner', async () => {
             const owner = await contract.owner({from: ownerOrSender})
-            assert.strictEqual(owner, ownerOrSender, "Contract owner is not the same")
+            assert.strictEqual(owner, ownerOrSender, 'Contract owner is not the same')
         })
 
-        it("should set a new owner", async () => {
-            const transferOwnership = await contract.transferOwnership(firsRecipient)
+        it('should set a new owner', async () => {
+            const transferOwnership = await contract.transferOwnership(firstRecipient)
             const newOwner = await contract.owner()
-            assert.strictEqual(newOwner, firsRecipient, "Contract owner has not been updated")
+            assert.strictEqual(newOwner, firstRecipient, 'Contract owner has not been updated')
         })
 
-        it("should log an LogOwnershipTransferred event", async () => {
+        it('should log an LogOwnershipTransferred event', async () => {
             const transferOwnership = await contract.transferOwnership(secondRecipient)
             const newOwner = await contract.owner()
             const ev = expectEvent(transferOwnership, 'LogOwnershipTransferred')
@@ -52,140 +52,155 @@ contract('Splitter', function(accounts) {
         })
     })
 
-    describe("Pause stuff:", async () => {
-        it("should set pause property to false", async () => {
+    describe('Pause stuff:', async () => {
+        it('should set pause property to false', async () => {
             const status = await contract.paused()
-            assert.strictEqual(status, false, "Contract owner is not the same")
+            assert.strictEqual(status, false, 'Contract owner is not the same')
         })
 
-        it("should not be able to set pause", async () => {
+        it('should not be able to set pause', async () => {
             try {
-                const status = await contract.pause({from: firsRecipient})
+                const status = await contract.pause({from: firstRecipient})
                 assert.isUndefined(status, 'Anyone can pause my contract')
             } catch(err) {
                 assert.include(err.message, 'revert', 'No revert if anyone kill my contract');
             }
         })
 
-        it("should be able to set pause", async () => {
+        it('should be able to set pause', async () => {
                 const pause = await contract.pause()
                 const status = await contract.paused()
                 assert.strictEqual(status, true, 'Owner is not able to pause the contract')
         })
 
-        it("should LogPause event", async () => {
+        it('should LogPause event', async () => {
             const pause = await contract.pause()
             const ev = expectEvent(pause, 'LogPause')
             expect(ev.args.whodunnit).to.equal(ownerOrSender)
         })
 
-        it("should LogUnpause event", async () => {
+        it('should LogUnpause event', async () => {
             const pause = await contract.pause()
             const ev = expectEvent(pause, 'LogPause')
             expect(ev.args.whodunnit).to.equal(ownerOrSender)
         })
     })
 
-    describe("Split public function:", async () => {
-        it("should fail if the owner/sender is the part of the split process", async () => {
+    describe('Split public function:', async () => {
+        it('should fail if the owner/sender is the part of the split process', async () => {
             try {
-                const split = await contract.split(firsRecipient, ownerOrSender, {from: ownerOrSender, value: 1})
+                const split = await contract.split(firstRecipient, ownerOrSender, {from: ownerOrSender, value: 1})
                 assert.isUndefined(split, 'The owner can take part in the split proccess')
             } catch(e) {
                 assert.include(e.message, 'revert', 'The owner cannot take part in the split proccess');
             }
         })
-        it("should fail if the recipients are the same", async () => {
+        it('should fail if the recipients are the same', async () => {
             try {
-                const split = await contract.split(firsRecipient, firsRecipient, {from: ownerOrSender, value: 1})
+                const split = await contract.split(firstRecipient, firstRecipient, {from: ownerOrSender, value: 1})
                 assert.isUndefined(split, 'The split process can accept same recipients')
             } catch(e) {
                 assert.include(e.message, 'revert', 'The split process cannot accept same recipients');
             }
         })
-        it("should fail if the the recipient is empty", async () => {
+        it('should fail if the the recipient is empty', async () => {
             try {
-                const split = await contract.split(firsRecipient, '0x0000000000000000000000000000000000000000', {from: ownerOrSender, value: 1})
+                const split = await contract.split(firstRecipient, '0x0000000000000000000000000000000000000000', {from: ownerOrSender, value: 1})
                 assert.isUndefined(split, 'The split process can accept empty recipients')
             } catch(e) {
                 assert.include(e.message, 'revert', 'The split process cannot accept empty recipients');
             }
         })
-        it("should fail because of amount = 0", async () => {
+        it('should fail because of amount = 0', async () => {
             try {
-                const split = await contract.split(firsRecipient, secondRecipient, {from: ownerOrSender, value: 0})
+                const split = await contract.split(firstRecipient, secondRecipient, {from: ownerOrSender, value: 0})
                 assert.isUndefined(split, 'The split process can accept 0 as msg.value')
             } catch(e) {
                 assert.include(e.message, 'revert', 'The split process cannot accept 0 as msg.value');
             }
         })
-        it("should fail because of odd amounts", async () => {
+        it('should fail because of odd amounts', async () => {
             try {
-                const split = await contract.split(firsRecipient, secondRecipient, {from: ownerOrSender, value: 1})
+                const split = await contract.split(firstRecipient, secondRecipient, {from: ownerOrSender, value: 1})
                 assert.isUndefined(split, 'The split process can accept odd amounts')
             } catch(e) {
                 assert.include(e.message, 'revert', 'The split process cannot accept odd amounts');
             }
         })
-        it("should not change recipients balances", async () => {
-            const firstInitial = await web3.eth.getBalance(firsRecipient)
+        it('should not change recipients balances', async () => {
+            const firstInitial = await web3.eth.getBalance(firstRecipient)
             const secondInitial = await web3.eth.getBalance(secondRecipient)
-            const split = await contract.split(firsRecipient, secondRecipient, {from: ownerOrSender, value: web3.toWei('4', 'ether')})
-            const firstFinal = await web3.eth.getBalance(firsRecipient)
+            const split = await contract.split(firstRecipient, secondRecipient, {from: ownerOrSender, value: web3.toWei('4', 'ether')})
+            const firstFinal = await web3.eth.getBalance(firstRecipient)
             const secondFinal = await web3.eth.getBalance(secondRecipient)
-            assert.strictEqual(firstInitial.toString(10), firstFinal.toString(10), "firsRecipient has a changed balance")
-            assert.strictEqual(secondInitial.toString(10), secondFinal.toString(10), "secondRecipient has a changed balance")
+            assert.strictEqual(firstInitial.toString(10), firstFinal.toString(10), 'firstRecipient has a changed balance')
+            assert.strictEqual(secondInitial.toString(10), secondFinal.toString(10), 'secondRecipient has a changed balance')
         })
-        it("should set 2 ehter to be withdraw for both the recipients", async () => {
-            const split = await contract.split(firsRecipient, secondRecipient, {from: ownerOrSender, value: web3.toWei('4', 'ether')})
-            const balances = await contract.balances(firsRecipient)
+        it('should set 2 ehter to be withdraw for both the recipients', async () => {
+            const split = await contract.split(firstRecipient, secondRecipient, {from: ownerOrSender, value: web3.toWei('4', 'ether')})
+            const balances = await contract.balances(firstRecipient)
             const balances2 = await contract.balances(secondRecipient)
-            assert.strictEqual(balances.toString(10), web3.toWei('2', 'ether').toString(10), "firsRecipient has not 2 ether ready for withdraw")
-            assert.strictEqual(balances2.toString(10), web3.toWei('2', 'ether').toString(10), "firsRecipient has not 2 ether ready for withdraw")
+            assert.strictEqual(balances.toString(10), web3.toWei('2', 'ether').toString(10), 'firstRecipient has not 2 ether ready for withdraw')
+            assert.strictEqual(balances2.toString(10), web3.toWei('2', 'ether').toString(10), 'firstRecipient has not 2 ether ready for withdraw')
         })
-        it("should fire LogSplit event", async () => {
-            const split = await contract.split(firsRecipient, secondRecipient, {from: ownerOrSender, value: web3.toWei('4', 'ether')})
+        it('should fire LogSplit event', async () => {
+            const split = await contract.split(firstRecipient, secondRecipient, {from: ownerOrSender, value: web3.toWei('4', 'ether')})
             const ev = expectEvent(split, 'LogSplit')
             expect(ev.args.from).to.equal(ownerOrSender)
-            expect(ev.args.firstRecipient).to.equal(firsRecipient)
+            expect(ev.args.firstRecipient).to.equal(firstRecipient)
             expect(ev.args.secondRecipient).to.equal(secondRecipient)
             expect(ev.args.amount.toString(10)).to.equal(web3.toWei('2', 'ether'))
         })
 
-        it("should find 0 ehter to be withdraw by thirdParty", async () => {
-            const split = await contract.split(firsRecipient, secondRecipient, {from: ownerOrSender, value: web3.toWei('4', 'ether')})
+        it('should find 0 ehter to be withdraw by thirdParty', async () => {
+            const split = await contract.split(firstRecipient, secondRecipient, {from: ownerOrSender, value: web3.toWei('4', 'ether')})
             const balances = await contract.balances(thirdParty)
-            assert.strictEqual(balances.toString(10), web3.toWei('0', 'ether').toString(10), "thirdParty has not 0 ether ready for withdraw")
+            assert.strictEqual(balances.toString(10), web3.toWei('0', 'ether').toString(10), 'thirdParty has not 0 ether ready for withdraw')
         })
     })
-    describe("Withdraw public function:", async () => {
-        it("should fire LogWithdraw event", async () => {
-            const split = await contract.split(firsRecipient, secondRecipient, {from: ownerOrSender, value: web3.toWei('4', 'ether')})
-            const withdraw = await contract.withdraw({from: firsRecipient})
+    describe('Withdraw public function:', async () => {
+        it('should fire LogWithdraw event', async () => {
+            const split = await contract.split(firstRecipient, secondRecipient, {from: ownerOrSender, value: web3.toWei('4', 'ether')})
+            const withdraw = await contract.withdraw({from: firstRecipient})
             const ev = expectEvent(withdraw, 'LogWithdraw')
-            expect(ev.args.from).to.equal(firsRecipient)
+            expect(ev.args.from).to.equal(firstRecipient)
             expect(ev.args.amount.toString(10)).to.equal(web3.toWei('2', 'ether'))
         })
-        it("should increase recipient balance", async () => {
-            const initial = await web3.eth.getBalance(firsRecipient)
-            const split = await contract.split(firsRecipient, secondRecipient, {from: ownerOrSender, value: web3.toWei('4', 'ether')})
-            const withdraw = await contract.withdraw({from: firsRecipient})
-            const final = await web3.eth.getBalance(firsRecipient)
-            assert.isAbove(final, initial, "firsRecipient balance is not greater than before the withdraw request")
+        it('should increase recipient balance', async () => {
+            const initial = await web3.eth.getBalance(firstRecipient)
+            const split = await contract.split(firstRecipient, secondRecipient, {from: ownerOrSender, value: web3.toWei('4', 'ether')})
+            const withdraw = await contract.withdraw({from: firstRecipient})
+            const final = await web3.eth.getBalance(firstRecipient)
+            assert.isAbove(final, initial, 'firstRecipient balance is not greater than before the withdraw request')
         })
-        it("should have final balance equal to the diff btw initial balance - sent - tx cost", async () => {
-            const senderInitial = await web3.eth.getBalance(ownerOrSender)
+        it('should have initial balance equal to the final balance + sent + fee)', async () => {
+            
+            const initial = await web3.eth.getBalance(ownerOrSender)
+            
+            const split = await contract.split(firstRecipient, secondRecipient, {from: ownerOrSender, value: web3.toWei('4', 'ether')})
 
-            const split = await contract.split(firsRecipient, secondRecipient, {from: ownerOrSender, value: web3.toWei('4', 'ether')})
+            const gasUsed = split.receipt.gasUsed
+            const gasPrice = await web3.eth.getTransaction(split.tx).gasPrice
+            const fee = gasPrice.times(gasUsed)
 
-            const splitGasUsed = split.receipt.gasUsed
-            const splitGasPrice = await web3.eth.getTransaction(split.tx).gasPrice
-            const splitTransactionCost = splitGasUsed * (web3.toWei(splitGasPrice, 'wei'))
+            const final = await web3.eth.getBalance(ownerOrSender)
 
-            const diff = senderInitial - web3.toWei('4', 'ether') - splitTransactionCost
-            const senderFinal = await web3.eth.getBalance(ownerOrSender)
+            assert.equal(initial.toString(10), final.plus(web3.toWei('4', 'ether')).plus(fee).toString(10), 'sender initial balance different from final balance + sent + fee')
+        })
+        it('should have initial balance equal to the final balance + sent + fee)', async () => {
+            
+            const initial = await web3.eth.getBalance(firstRecipient)
+            
+            const split = await contract.split(firstRecipient, secondRecipient, {from: ownerOrSender, value: web3.toWei('4', 'ether')})
+            const withdraw = await contract.withdraw({from: firstRecipient})
 
-            assert.equal(senderFinal, diff, "sender (initial balance - sent - transaction cost) is not equal to the final balance")
+            const gasUsed = withdraw.receipt.gasUsed
+            const gasPrice = await web3.eth.getTransaction(withdraw.tx).gasPrice
+            const fee = gasPrice.times(gasUsed)
+
+            const final = await web3.eth.getBalance(firstRecipient)
+
+            assert.equal(final.toString(10), initial.plus(web3.toWei('2', 'ether')).minus(fee).toString(10), 'sender initial balance different from final balance + sent + fee')
         })
     })
 })
